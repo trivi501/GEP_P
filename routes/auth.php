7843ec950770d1121020d0da2a081b995e9532dc
Jorge Campos <trivijorge12@gmail.com>
2026-05-20 02:07:18 -0600

    Fix missing VerifyEmailController import

--- routes/auth.php
+++ routes/auth.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
