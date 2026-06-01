a46802d3ad7a41153787fc95b39c1d9937abb526
Jorge Fang <jorgefang@Jorges-MacBook-Pro.local>
2026-06-01 09:33:10 -0600

    QUITAR fondo imagen recibo-pdf

--- resources/views/pagos/recibo-pdf.blade.php
+++ resources/views/pagos/recibo-pdf.blade.php
@@ -11,12 +11,7 @@
             font-size: 9pt;
             color: #1a1a1a;
             line-height: 1.4;
-            background-image: url("{{ public_path('img/formatoRecibo.jpg') }}");
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
         }
-
         .header { padding: 4px 0; border-bottom: 2px double #333; margin-bottom: 6px; text-align: center; }
         .header h1 { font-size: 11pt; text-transform: uppercase; letter-spacing: 1px; margin: 0; }
         .header h2 { font-size: 9pt; margin: 0; }
