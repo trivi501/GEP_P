6adb4b7c3350ab056abf3c2cb9ab8a3b97fd3246
Jorge Campos <trivijorge12@gmail.com>
2026-06-03 00:48:56 -0600

    Add Spanish translations and set locale to es

---
+++ lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => '¡Su contraseña ha sido restablecida!',
+    'sent' => '¡Le hemos enviado el enlace de restablecimiento de contraseña por correo!',
+    'throttled' => 'Por favor espere antes de intentar de nuevo.',
+    'token' => 'El token de restablecimiento de contraseña es inválido.',
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
+];
