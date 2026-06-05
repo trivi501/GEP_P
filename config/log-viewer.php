611c1e9fba8a6b9c006cba24c5f8604a86d7cc29
Jorge Campos <trivijorge12@gmail.com>
2026-06-05 01:31:13 -0600

    Disable require_auth_in_production for log-viewer

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -20,7 +20,7 @@ return [
 
     'api_only' => env('LOG_VIEWER_API_ONLY', false),
 
-    'require_auth_in_production' => true,
+    'require_auth_in_production' => false,
 
     /*
     |--------------------------------------------------------------------------
