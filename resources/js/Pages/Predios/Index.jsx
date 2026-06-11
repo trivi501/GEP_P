7069721e075d21884798d52d8b703d2cf61bb5e9
Jorge Campos <trivijorge12@gmail.com>
2026-06-10 23:02:46 -0600

    Editar button in list requires predio-editar permission

--- resources/js/Pages/Predios/Index.jsx
+++ resources/js/Pages/Predios/Index.jsx
@@ -302,12 +302,12 @@ const esNormal = (predio) => predio.id_estatus === 1;
                                                                 >
                                                                     <svg className="w-5 h-5 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>
                                                                 </Link>
-                                                                {can('predios-show') && (
-                                                                <Link
-                                                                    href={route('predios.edit', predio.id)}
-                                                                    className="text-yellow-600 hover:text-yellow-900"
-                                                                    title="Editar"
-                                                                >
+                                                            {can('predio-editar') && (
+                                                            <Link
+                                                                href={route('predios.edit', predio.id)}
+                                                                className="text-yellow-600 hover:text-yellow-900"
+                                                                title="Editar"
+                                                            >
                                                                     <svg className="w-5 h-5 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>
                                                                 </Link>
                                                                 )}
