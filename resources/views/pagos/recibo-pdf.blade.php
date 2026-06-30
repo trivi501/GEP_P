a3923709ebb9971e0179fed994a514cf6e8bdbe7
Jorge Fang <jorgefang@Jorges-MacBook-Pro.local>
2026-06-30 11:03:18 -0600

    Remove empty td in recibo-pdf frente

--- resources/views/pagos/recibo-pdf.blade.php
+++ resources/views/pagos/recibo-pdf.blade.php
@@ -132,7 +132,6 @@
                     <td width="30%" style="text-align: right; font-size: 7pt;">Fecha: {{ \Carbon\Carbon::parse($pago->fecha)->format('d/m/Y H:i') }}</td>
                 </tr>
                 <tr>
-                    <td></td>
                     <td width="30%" style="text-align: right; font-size: 6.5pt;">Cajero: {{ $pago->user?->username ?? $pago->user?->name ?? '—' }}</td>
                 </tr>
             </table>
@@ -233,7 +232,6 @@
                     <td width="30%" style="text-align: right; font-size: 7pt;">Fecha: {{ \Carbon\Carbon::parse($pago->fecha)->format('d/m/Y H:i') }}</td>
                 </tr>
                 <tr>
-                    <td></td>
                     <td width="30%" style="text-align: right; font-size: 6.5pt;">Cajero: {{ $pago->user?->username ?? $pago->user?->name ?? '—' }}</td>
                 </tr>
             </table>
